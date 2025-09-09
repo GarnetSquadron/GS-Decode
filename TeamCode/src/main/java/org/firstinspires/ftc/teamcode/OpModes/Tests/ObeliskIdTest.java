0611028fc8297825cb0951107624f86f319482c9
DJturntables <darthdj08@gmail.com>
2025-09-09 15:29:56 -0400

    Added code for identifying the obelisk apriltags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/ObeliskIdTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.OpModes.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Vision.aprilTags.ObeliskIdentifier;
+
+import java.lang.annotation.Inherited;
+
+@TeleOp(group = "test")
+public class ObeliskIdTest extends OpMode
+{
+    ObeliskIdentifier obeliskIdentifier;
+    @Override
+    public void init()
+    {
+        obeliskIdentifier = new ObeliskIdentifier(hardwareMap);
+    }
+
+    @Override
+    public void loop()
+    {
+        obeliskIdentifier.telemetryAprilTag(telemetry);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/aprilTags/ObeliskIdentifier.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.Vision.aprilTags;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class ObeliskIdentifier
+{
+    public enum ObeliskPatterns
+    {
+        GPP(21),
+        PGP(22),
+        PPG(23),
+        NOTHING_DETECTED(-1);
+        private final int value; // Field to store the custom value
+
+        private ObeliskPatterns(int value) { // Private constructor
+            this.value = value;
+        }
+    }
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    private AprilTagProcessor aprilTagProcessor;
+
+    private VisionPortal visionPortal;
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    public ObeliskIdentifier(HardwareMap hardwareMap) {
+
+        // Create the AprilTag processor the easy way.
+        aprilTagProcessor = AprilTagProcessor.easyCreateWithDefaults();
+
+        // Create the vision portal the easy way.
+        if (USE_WEBCAM) {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    hardwareMap.get(WebcamName.class, "Webcam 1"), aprilTagProcessor);
+        } else {
+            visionPortal = VisionPortal.easyCreateWithDefaults(
+                    BuiltinCameraDirection.BACK, aprilTagProcessor);
+        }
+
+    }   // end method initAprilTag()
+
+    public ObeliskPatterns getObelisk(){
+        for(AprilTagDetection detection: aprilTagProcessor.getDetections()){
+            for(ObeliskPatterns tag: ObeliskPatterns.values()){
+                if(tag.value==detection.id){
+                    return tag;
+                }
+            }
+        }
+        return ObeliskPatterns.NOTHING_DETECTED;
+    }
+
+    /**
+     * Add telemetry about AprilTag detections.
+     */
+    public void telemetryAprilTag(Telemetry telemetry) {
+
+        List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }   // end for() loop
+
+        // Add "key" information to telemetry
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }   // end method telemetryAprilTag()
+
+}
