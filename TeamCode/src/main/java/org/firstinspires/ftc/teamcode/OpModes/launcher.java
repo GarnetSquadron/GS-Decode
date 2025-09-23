19412e29c5f687694a79f0d1e6d4dd26d629a633
inventorlord <charlie.fomby8@gmail.com>
2025-09-23 19:36:57 -0400

    can change motor power by 0.1 when a is pressed on a connected gamepad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
@@ -14,19 +14,31 @@ import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 public class launcher extends OpMode {
     RAWMOTOR motor1;
     //RAWMOTOR motor2;
-    int power = 0;
+    double power = 0;
+    boolean wasPressed = false;
     @Override
     public void init() {
         motor1 = new RAWMOTOR(hardwareMap, "launcherMotor1");
         //motor2 = new RAWMOTOR(hardwareMap, "launcherMotor2");
-
     }
     public void loop() {
         motor1.setPower(power);
         //motor2.setPower(power);
         if (gamepad1.a) {
-           power += 0.1;
+            if (wasPressed == false) {
+                power += 0.1;
+                telemetry.addLine("a pressed");
+                wasPressed = true;
+            }
+
         }
+        else {
+            wasPressed = false;
+        }
+
+        telemetry.addData("power",power);
+        telemetry.addData("motor power", motor1.getPower());
+
 
     }
 }
