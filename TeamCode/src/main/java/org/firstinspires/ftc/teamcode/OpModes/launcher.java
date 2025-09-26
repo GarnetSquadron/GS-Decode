325381eccfd1fe37e9f939e03759ae9488204ad5
inventorlord <charlie.fomby8@gmail.com>
2025-09-26 18:25:00 -0400

    added servo position and a servo object to the launcher class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
@@ -4,40 +4,44 @@ import static java.lang.Thread.sleep;
 
 import android.webkit.JavascriptInterface;
 
+
 import com.acmerobotics.roadrunner.SleepAction;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 @TeleOp(name = "launcher")
 public class launcher extends OpMode {
+    Servo angleServo;
     RAWMOTOR motor1;
-    //RAWMOTOR motor2;
+    RAWMOTOR motor2;
     double power = 0;
+
+    double servoPosition = 0.5;
     boolean wasPressed = false;
     @Override
     public void init() {
+
+        angleServo = hardwareMap.get(Servo.class, "angleServo");
         motor1 = new RAWMOTOR(hardwareMap, "launcherMotor1");
-        //motor2 = new RAWMOTOR(hardwareMap, "launcherMotor2");
+        motor2 = new RAWMOTOR(hardwareMap, "launcherMotor2");
     }
     public void loop() {
-        motor1.setPower(power);
-        //motor2.setPower(power);
-        if (gamepad1.a) {
-            if (wasPressed == false) {
-                power += 0.1;
-                telemetry.addLine("a pressed");
-                wasPressed = true;
-            }
 
-        }
-        else {
-            wasPressed = false;
-        }
+        if (gamepad1.b) power = 1;
+        else power = 0;
+
+        motor1.setPower(power);
+        motor2.setPower(power / -1);
+
+        angleServo.setPosition(servoPosition);
 
         telemetry.addData("power",power);
+
         telemetry.addData("motor power", motor1.getPower());
+        telemetry.addData("servo angle", angleServo.getDirection());
 
 
     }
