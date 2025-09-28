71c1c6f7252f387e0f8418418f26e284eec98607
DJturntables <darthdj08@gmail.com>
2025-09-27 21:34:18 -0400

    put the test in a new file to test it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TouchSensorDelayTest2.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.teamcode.time.TIME;
+
+@TeleOp(name = "delayTest")
+public class TouchSensorDelayTest2 extends OpMode
+{
+    ColorSensor sensor;
+    double loopStartTime;
+    double deltaTime = 0;
+    double distance = 0;
+    @Override
+    public void init()
+    {
+        sensor = hardwareMap.get(ColorSensor.class, "colorSensor");
+    }
+
+    @Override
+    public void loop()
+    {
+        loopStartTime = TIME.getTime();
+        if(gamepad1.a){
+            distance = sensor.blue();
+        }
+        deltaTime = TIME.getTime()-loopStartTime;
+        telemetry.addData("loopTime",deltaTime);
+        telemetry.addData("distance", distance);
+    }
+}
