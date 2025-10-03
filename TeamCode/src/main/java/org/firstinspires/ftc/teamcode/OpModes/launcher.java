a3a38fdab0bd41f332f60e5f7122707a8636e0da
DJturntables <darthdj08@gmail.com>
2025-10-03 19:20:19 -0400

    added import that was weirdly missing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/launcher.java
@@ -1,17 +1,10 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import static java.lang.Thread.sleep;
-
-import android.webkit.JavascriptInterface;
-
-
-import com.acmerobotics.roadrunner.SleepAction;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import org.firstinspires.ftc.teamcode.ExtraMath;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.ExtraMath;
 import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 @TeleOp(name = "launcher")
 public class launcher extends OpMode {
