88804c1f23985c90d02271d8662c2a71c05886cb
inventorlord <charlie.fomby8@gmail.com>
2025-10-08 11:41:40 -0400

    added some stuff to launcher calss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launcher.java
@@ -7,14 +7,23 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 
 public class launcher {
-    static Servo angleServo;
-    static RAWMOTOR motor1;
-    static RAWMOTOR motor2;
-    double power = 0;
 
-    public static void init(){
+    Servo angleServo;
+    RAWMOTOR motor1;
+    RAWMOTOR motor2;
+    double power = 0;
+    public void setAngle(double angle, double tmpScaleFactor){
+        angleServo.setPosition(angle * tmpScaleFactor);
+    }
+
+    public void setPower(double power) {
+        motor2.setPower(power);
+        motor1.setPower(power);
+    }
+
+    public void init(){
         angleServo = hardwareMap.get(Servo.class, "angleServo");
         motor1 = new RAWMOTOR(hardwareMap, "launcherMotor1");
         motor2 = new RAWMOTOR(hardwareMap, "launcherMotor2");
-
+        motor2.reverseMotor();
     }}
