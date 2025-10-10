e1da9ed875097bd3701f04c13f48c1468ad7d776
inventorlord <charlie.fomby8@gmail.com>
2025-10-10 19:50:34 -0400

    added stuff t0 calculator test class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AimCalculatorTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import static org.firstinspires.ftc.teamcode.pathing.pedroPathing.Tuning.follower;
+
+import com.arcrobotics.ftclib.controller.PController;
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.AngleFinder;
+import org.firstinspires.ftc.teamcode.Dimensions.FieldDimensions;
+import org.firstinspires.ftc.teamcode.launcher;
+
+@TeleOp(name = "aimCalc" )
+public class AimCalculatorTest extends OpMode {
+    launcher launcher = new launcher();
+        public void aimAtGoal(double[] goalPos, double[] botPos, int vel) {
+            double distance = Math.toRadians( Math.sqrt(((((goalPos[0] + goalPos[1])*2)+((botPos[0]-botPos[1])*2)))));
+            double angle = AngleFinder.getOptimumAngle(vel,distance);
+            launcher.setAngle(angle,0);
+            double rotAngle = Math.atan((goalPos[1] - botPos[1]) /(goalPos[0]-botPos[0]));
+            launcher.setTurretRotation(rotAngle);
+        }
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.a) {
+            double[] botPos = new double[] {follower.getPose().getX(), follower.getPose().getY()};
+            aimAtGoal(FieldDimensions.goalPositionRed,botPos,6);
+
+        }
+    }
+}
