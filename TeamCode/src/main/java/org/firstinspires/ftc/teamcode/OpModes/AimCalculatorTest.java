e7a0b745819c0273b8decd9ea2368338726082b2
DJturntables <darthdj08@gmail.com>
2025-10-14 19:19:11 -0400

    fixed follower in aimcalc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AimCalculatorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AimCalculatorTest.java
@@ -2,36 +2,49 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import static org.firstinspires.ftc.teamcode.pathing.pedroPathing.Tuning.follower;
 
+import com.bylazar.configurables.PanelsConfigurables;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.AngleFinder;
 import org.firstinspires.ftc.teamcode.Dimensions.FieldDimensions;
 import org.firstinspires.ftc.teamcode.Launcher;
+import org.firstinspires.ftc.teamcode.pathing.pedroPathing.Constants;
 
 @TeleOp(name = "aimCalc" )
 public class AimCalculatorTest extends OpMode {
     Launcher launcher;
-        public void aimAtGoal(double[] goalPos, double[] botPos, int vel) {
+        public double aimAtGoal(double[] goalPos, double[] botPos, int vel) {
             double distance = Math.toRadians( Math.sqrt(((((goalPos[0] + goalPos[1])*2)+((botPos[0]-botPos[1])*2)))));
             double angle = AngleFinder.getOptimumAngle(vel,distance);
             launcher.setAngle(angle,0);
             double rotAngle = Math.atan((goalPos[1] - botPos[1]) /(goalPos[0]-botPos[0]));
             launcher.setTurretRotation(rotAngle);
+            return angle;
         }
 
 
     @Override
     public void init() {
+        if (follower == null) {
+            follower = Constants.createFollower(hardwareMap);
+            PanelsConfigurables.INSTANCE.refreshClass(this);
+        } else {
+            follower = Constants.createFollower(hardwareMap);
+        }
+
+        follower.setStartingPose(new Pose());
         launcher = new Launcher(hardwareMap);
     }
 
     @Override
     public void loop() {
+        follower.update();
         if (gamepad1.a) {
             double[] botPos = new double[] {follower.getPose().getX(), follower.getPose().getY()};
-            aimAtGoal(FieldDimensions.goalPositionRed,botPos,6);
+            telemetry.addData("angle",aimAtGoal(FieldDimensions.goalPositionRed,botPos,6));
+            telemetry.update();
         }
         if (gamepad1.y) {
             launcher.setPower(1);
