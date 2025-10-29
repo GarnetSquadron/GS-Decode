f748d03dbe8125fa8a0fc541e45a8f80bcf1eddc
inventorlord <charlie.fomby8@gmail.com>
2025-10-28 20:52:00 -0400

    some stuff with launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TestLauncher.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.ExtraMath;
+import org.firstinspires.ftc.teamcode.LauncherPid;
 import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 @TeleOp(name = "launcher")
 public class TestLauncher extends OpMode {
@@ -12,7 +13,7 @@ public class TestLauncher extends OpMode {
     RAWMOTOR motor1;
     RAWMOTOR motor2;
     double power = 0;
-
+    double target = 0;
     double servoPosition = 0;
     //temp
     boolean leftWasPressed = false;
@@ -25,8 +26,10 @@ public class TestLauncher extends OpMode {
         motor2 = new RAWMOTOR(hardwareMap, "launcherMotor2");
     }
     public void loop() {
+
         ExtraMath.Clamp(servoPosition,1,0);
         //temp
+        /*
         leftWasPressed = false;
         rightWasPressed = false;
         if (gamepad1.dpad_left & !leftWasPressed) {
@@ -55,7 +58,17 @@ public class TestLauncher extends OpMode {
 
         telemetry.addData("motor power", motor1.getPower());
         telemetry.addData("servo position", angleServo.getPosition());
+        */
+        telemetry.addData("power ",motor1.getPower());
+        telemetry.addData("velocity ",motor1.getEncoder().getVelocity());
 
+        if (gamepad1.a) {target +=0.1;}
+        if (gamepad1.b) {target -=0.1;}
+        telemetry.addData("target",target);
+        telemetry.addData("a pressed",true);
+        motor1.setPower(LauncherPid.setPid(motor1.getEncoder().getVelocity(),target,0.5));
+        telemetry.addData("y stick",gamepad2.left_stick_y);
 
+        telemetry.update();
     }
 }
