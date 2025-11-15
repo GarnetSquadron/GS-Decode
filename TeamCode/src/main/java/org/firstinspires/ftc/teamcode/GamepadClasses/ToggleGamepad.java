4973dd479026fe5160f12cec21fda6ab6e6e00d8
DJturntables <darthdj08@gmail.com>
2025-11-15 11:58:02 -0500

    Wrote a really good system for the ToggleGamepad.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadClasses/ToggleGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadClasses/ToggleGamepad.java
@@ -2,15 +2,47 @@ package org.firstinspires.ftc.teamcode.GamepadClasses;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.teamcode.inputmodifiers.BooleanToggler;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * planning on doing some hashmap stuff with this but not rn
+ */
 public class ToggleGamepad extends BetterControllerClass
 {
-    public BooleanToggler aToggle, bToggle, xToggle, yToggle, leftTriggerToggle, rightTriggerToggle, leftBumperToggle, rightBumperToggle;
+    String[] buttonNames = new String[]{"a", "b", "x", "y"};
+    public Map<String, Boolean> toggles = new HashMap<>();
+    public Map<String,Boolean> prevValues = new HashMap<>();
 
     public ToggleGamepad(Gamepad gamepad)
     {
         super(gamepad);
+        for(String buttonName: buttonNames){
+            toggles.put(buttonName,false);
+            prevValues.put(buttonName,false);
+        }
+    }
+
+    /**
+     * iterates over the buttons, and
+     */
+    public void update()
+    {
+        for(String buttonName:buttonNames){
+            boolean toggle = Boolean.TRUE.equals(toggles.get(buttonName));
+            boolean prevValue = Boolean.TRUE.equals(prevValues.get(buttonName));
+            try
+            {
+                boolean buttonPressed = (Boolean) Gamepad.class.getField(buttonName).get(gamepad);
+                boolean valHasChanged = prevValue^buttonPressed;
+                toggles.put(buttonName,valHasChanged^toggle);
+                prevValues.put(buttonName,buttonPressed);
+
+
+            } catch (NoSuchFieldException | IllegalAccessException ignored)
+            {
+            }
+        }
     }
 
 
