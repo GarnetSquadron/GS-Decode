c99afe54f9ccdf927090f675c52c01860722f72c
DJturntables <darthdj08@gmail.com>
2025-11-18 15:53:38 -0500

    modified the field dimensions a little and added a position for when the robot is against the goal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dimensions/FieldDimensions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dimensions/FieldDimensions.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.Dimensions;
 
+import com.pedropathing.geometry.Pose;
+
 public class FieldDimensions
 {
     public static double minGoalHeight = 38.75;//0.9645 meters
     public static double maxGoalHeight = 15;// 1.3655 meters
 
     //update later with real positions
-    public static double[] fieldCenter = {72,72};
-    public static double[] goalPositionRed = {130,130};
-    public static double[] goalPositionBlue = {15,130};
+    public static double[] fieldCenter = {72,72};//(0,0) is the corner of the field
+    public static double[] goalPositionRed = {135,135};
+    public static double[] goalPositionBlue = {15,135};
+    public static Pose botTouchingRedGoal = new Pose(123,123,36);//The position when the robot is contacting the side of the goal. Good starting position for tests I think
 }
