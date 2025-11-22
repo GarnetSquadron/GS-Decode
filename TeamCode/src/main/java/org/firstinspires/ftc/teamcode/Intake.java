421f49c5368ce2bae17a4379b8b51bc3af577f30
DJturntables <darthdj08@gmail.com>
2025-11-22 17:52:18 -0500

    added the code for the servo kicker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.hardwareClasses.motors.RAWMOTOR;
 
 public class Intake {
+    Servo servoKicker;
     RAWMOTOR intakeMotor;
 
     public Intake(HardwareMap hardwareMap) {
         intakeMotor = new RAWMOTOR(hardwareMap, "intakeMotor");
+        servoKicker = hardwareMap.get(Servo.class, "servoKicker");
         intakeMotor.reverseMotor();
     }
 
@@ -17,5 +20,11 @@ public class Intake {
     }
     public void stop() {
         intakeMotor.stop();
+    }
+    public void kickBall(){
+        servoKicker.setPosition(0.64);
+    }
+    public void unKick(){
+        servoKicker.setPosition(0.2);
     }
 }
