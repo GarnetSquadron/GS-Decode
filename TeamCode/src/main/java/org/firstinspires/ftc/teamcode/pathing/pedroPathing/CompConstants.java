0239e4d04750574c1ab0273cd43ab25bfe66232c
DJturntables <darthdj08@gmail.com>
2025-11-22 17:51:33 -0500

    Changed the odo positions for the new design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/pedroPathing/CompConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/pedroPathing/CompConstants.java
@@ -52,8 +52,8 @@ public class CompConstants
             .yVelocity(52.612);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(3)//TODO: Check this and figure out which center it is
-            .strafePodX(-6)
+            .forwardPodY(4.5)//TODO: Check this and figure out which center it is
+            .strafePodX(0)
             .distanceUnit(DistanceUnit.INCH)
             .hardwareMapName("pinpoint")
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
