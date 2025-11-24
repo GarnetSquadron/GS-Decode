b688020de808e09284a1243859d1a90a445af632
inventorlord <charlie.fomby8@gmail.com>
2025-11-24 15:06:54 -0500

    pid for flywheel changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/PIDMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/PIDMath.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.controllers;
 
-public class TurretMath {
+public class PIDMath {
     double x, y, xv, yv, xa, ya;
     public double angularVelocity(double deltaX, double deltaY, double xVel, double yVel){
         double aV = (yVel*deltaX-xVel*deltaY)/((deltaY*deltaY)+(deltaX*deltaX));
