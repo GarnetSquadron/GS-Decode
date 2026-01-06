d6751249413dc22db94716a41e21d25cc3132f41
DJturntables <darthdj08@gmail.com>
2026-01-06 18:26:35 -0500

    implemented the new model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurelyCalculators/TrajectoryMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PurelyCalculators/TrajectoryMath.java
@@ -3,18 +3,47 @@ package org.firstinspires.ftc.teamcode.PurelyCalculators;
 import org.firstinspires.ftc.teamcode.Dimensions.FieldDimensions;
 import org.firstinspires.ftc.teamcode.Dimensions.RobotDimensions;
 import org.firstinspires.ftc.teamcode.HardwareControls.Launcher;
+import org.firstinspires.ftc.teamcode.PurelyCalculators.ExtraMath.Complex;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TrajectoryMath
 {
+
+
+    /**
+     * quadratic curve approximation parameters for
+     * A+B*theta+Cy+D*theta*y+Ey^2
+     */
+    static double A = -58.97183942365856,B=-0.03645626379853637,C=399.981124985381,D=-2.032211274012549,E=0.004655773262312472;
+    public static double getM(double flywheelVel){
+        return A+B*flywheelVel+E*flywheelVel*flywheelVel;
+    }
+    public static double getN(double flywheelVel){
+        return C+D*flywheelVel;
+    }
+    //https://www.desmos.com/calculator/sxcfveqbri
+    public static double a(double M){
+        return M*M*g*g/4;
+    }
+    public static double b(double M,double N,double X,double Y){
+        return g*N*M*X+g*M*M*Y-g*g*X*X/4;
+    }
+    public static double c(double M,double N,double X,double Y){
+        return N*N*X*X+2*N*M*Y*X+M*M*Y*Y-Y*X*X*g;
+    }
+    public static double d(double X,double Y){
+        return -Y*Y*X*X-X*X*X*X;
+    }
     /**
      * needs to be somewhere between minGoalHeight and maxGoalHeight, so why not the average
      */
     final static public double targetHeight = (FieldDimensions.maxGoalHeight+FieldDimensions.minGoalHeight)/2.0-RobotDimensions.Hood.approxBallExitHeight;//The height difference is the height of the goal-the height of the ball as it exits
     public final static double g = 386.09;//9.8 m/s^2=386.09in/s
 
-    static final double a = g*g/4;
+    //static final double a = g*g/4;
 
     /**
      * calculate both the angles (in radians of course)
@@ -24,17 +53,27 @@ public class TrajectoryMath
      */
     public static double[] getAngles(double vel,double distance)
     {
-        double b = g*targetHeight-vel*vel;
-        double c = distance*distance+targetHeight*targetHeight;
-        double[] tSquared = ExtraMath.quadraticFormula(a,b,c);
-        if(tSquared.length==2){
-            //not sure if stream is the fastest option but its simpler
-            return Arrays.stream(tSquared)//solves a quadratic in terms of t^2
-                    .map(Math::sqrt)//gets t from t^2 (we know t is positive because it is the time after launch, not before)
-                    .map(t -> getAngleFromTime(t, distance))//gets the angle from the time.
-                    .toArray();//turns it back to an arrays
+
+        double M = getM(vel);
+        double N = getN(vel);
+        double a = a(M);
+        double b = b(M, N, distance, targetHeight);
+        double c = c(M, N, distance, targetHeight);
+        double d = d(distance, targetHeight);
+        Complex[] solutions = ExtraMath.solveCubic(a, b, c, d);
+        // Filter using Java Streams
+        Complex[] realSolutions = Arrays.stream(solutions)
+                .filter(Complex::isPurelyReal)
+                .toArray(Complex[]::new);
+        double[] angles = new double[realSolutions.length];
+        for (int i = 0; i < realSolutions.length; i++)
+        {
+            double t = Math.sqrt(realSolutions[i].a);
+            double slope = (g * t / 2 + targetHeight) / distance;
+            angles[i] = Math.atan(slope);
         }
-        else return new double[] {};
+        return angles;
+
     }
     /**
      * chooses the optimum angle (rn if theres a choice it just chooses the higher one by default because that one is more fun)
@@ -44,16 +83,11 @@ public class TrajectoryMath
      */
     public static double getOptimumAngle(double vel,double distance){
         double[] angles = getAngles(vel,distance);
-        if(angles.length==0){
-            return -1;// this means that the angle is impossible. In this case because the flywheel speed is too low
+        for(int i=0;i<angles.length;i++){
+
         }
-        else if(!isInAngleRange(angles[1])){
-            if(!isInAngleRange(angles[0])){
-                return -1;// angle is impossible because the angle is outside of the
-            }
-            else return angles[0];//if large angle
-        }
-        else return angles[1];//default to the larger angle because it is more fun
+        return angles[0];
+        //else return angles[1];//default to the larger angle because it is more fun
     }
      static boolean isInAngleRange(double angle){
         return angle<=Math.toRadians(RobotDimensions.Hood.maxAngle)&&angle>=Math.toRadians(RobotDimensions.Hood.minAngle);
