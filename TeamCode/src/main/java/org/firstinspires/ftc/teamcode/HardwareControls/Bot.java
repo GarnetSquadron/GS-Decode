707576d890df72aa2a5522dde1bbc2dd53f4223c
Anthony <154084304+AnthonyZFu@users.noreply.github.com>
2026-01-26 12:40:20 -0500

    Update Bot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareControls/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareControls/Bot.java
@@ -244,7 +244,7 @@ public class Bot
                 }
                 case SPINNING_UP:{
                     intake.stop();
-                    if (launcher.spinFlyWheelWithinRange(velBounds[0],velBounds[1])&&getElapsedTime()>0.1)//wait for it to be in the right range
+                    if (launcher.spinFlyWheelWithinRange(velBounds[0],velBounds[1])&&getElapsedTime()>0.3)//wait for it to be in the right range
                     {
                         launchPhase = LaunchPhase.GATE_OPENING;
                         phaseStartTime = TIME.getTime();
